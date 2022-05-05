27d134650a86c09bde12ad85bf281df3f6dd4174
Winnie <wrm956@gmail.com>
2022-05-04 21:18:22 -0400

    commit tree bf50209acd6e4d2910c37101f59407d0d04dc87f

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    let newArray = [...cats]
+    newArray.pop()
+    return newArray
+}
+
+function removeFirstCat() {
+    let byeCat = [...cats]
+    byeCat.shift()
+    return byeCat
+}
